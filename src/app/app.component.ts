cecc883140f43e3812dade87e5d541443bddb851
Revillacks <revillacks@MacBook-Pro-de-Revillacks.local>
2024-10-17 01:55:10 -0600

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component'; // Ensure this path is correct and the component is properly exported
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pokeApp';
+  deferredPrompt: any;
+  showInstallButton = false;
+  isAppInstalled = false;
+  
+constructor(private http: HttpClient) {
+    // Evento de instalación
+    window.addEventListener('appinstalled', () => {
+      this.isAppInstalled = true;
+      this.showInstallButton = false;
+    });
+  }
+
+  // Método para instalar la PWA
+  installPWA() {
+    if (this.isAppInstalled) {
+      alert('La aplicación ya está instalada.');
+    } else if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        this.deferredPrompt = null;
+      });
+    }
+  }
 }
+
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,12 +8,7 @@ import { Component, HostListener, signal } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  isShrunk = signal(false);
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    this.isShrunk.set(scrollTop > 50);
-  }
+
 
 }
